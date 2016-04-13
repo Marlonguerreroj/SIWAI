81feb5ab3d5b14ce3bc0566f4aba17d97d684824
aleja_000 <aleja_000@172.18.22.39>
2016-04-13 08:45:00 -0500

    Eliminacion de la caperta "servlet"

--- SIWAI/src/java/co/edu/ufps/siwai/controlador/ControladorEmpleado.java
+++ SIWAI/src/java/co/edu/ufps/siwai/controlador/ControladorEmpleado.java
@@ -1,13 +1,14 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.edu.ufps.siwai.controlador.servlet;
+package co.edu.ufps.siwai.controlador;
 
 import co.edu.ufps.siwai.modelo.fachada.Fachada;
-import co.edu.ufps.siwai.modelo.mysql.dto.SucursalDTO;
+import co.edu.ufps.siwai.modelo.mysql.dto.EmpleadoDTO;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,8 +20,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author MarlonGuerrero
  */
-@WebServlet(name = "ControladorSucursal", urlPatterns = {"/ControladorSucursal"})
-public class ControladorSucursal extends HttpServlet {
+@WebServlet(name = "ControladorEmpleado", urlPatterns = {"/ControladorEmpleado"})
+public class ControladorEmpleado extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -31,33 +32,37 @@ public class ControladorSucursal extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void registrarSucursal(HttpServletRequest request, HttpServletResponse response)
+    protected void registrarEmpleado(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
         String codigo = request.getParameter("codigo");
+        String dni = request.getParameter("dni");
         String nombre = request.getParameter("nombre");
-        int telefono = Integer.valueOf(request.getParameter("telefono"));
+        String apellido = request.getParameter("apellido");
+        String telefono = request.getParameter("telefono");
+        String celular = request.getParameter("celular");
+        String contrasena = request.getParameter("contrasena");
         String email = request.getParameter("email");
-        String paginaWeb = request.getParameter("paginaWeb");
         String direccion = request.getParameter("direccion");
-        String ciudad = request.getParameter("ciudad");
-        String pais = request.getParameter("pais");
+        String fIngreso = request.getParameter("fIngreso");
+        String cargo = request.getParameter("sel2");
+        String sucursal = request.getParameter("sel1");
         Fachada fachada = new Fachada();
         String mensaje;
         boolean exito = false;
         try {
-            exito = fachada.registrarSucursal(codigo, nombre, telefono, email, paginaWeb, direccion, ciudad, pais);
+            exito = fachada.registrarEmpleado(codigo, dni, nombre, apellido, telefono, celular, contrasena, email, direccion, fIngreso, cargo, sucursal);
         } catch (Exception ex) {
             mensaje = "Error en la conexion a la base de datos";
         }
         if (exito) {
-            mensaje = "Sucursal registrada exitosamente";
+            mensaje = "Empleado registrado exitosamente";
         } else {
-            mensaje = "Existe otra sucursal con el codigo ingresado";
+            mensaje = "Existe otro empleado con el DNI ingresado";
         }
-        request.getSession().setAttribute("msjRS", mensaje);
-        response.sendRedirect("/SIWAI/Seccion/Sucursal/registrar.jsp");
+        request.getSession().setAttribute("msjRE", mensaje);
+        response.sendRedirect("/SIWAI/Seccion/Empleado/registrar.jsp");
     }
 
     /**
@@ -69,25 +74,51 @@ public class ControladorSucursal extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void consultarSucursal(HttpServletRequest request, HttpServletResponse response)
+    protected void consultarEmpleado(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         request.setCharacterEncoding("UTF-8");
         String buscarPor = request.getParameter("sel");
         String informacion = request.getParameter("informacion");
         Fachada fachada = new Fachada();
-        ArrayList<SucursalDTO> lista = null;
+        ArrayList<EmpleadoDTO> lista = null;
         try {
-            lista = fachada.consultarSucursal(buscarPor, informacion);
+            lista = fachada.consultarEmpleado(buscarPor, informacion);
         } catch (Exception e) {
-            request.getSession().setAttribute("msjCS", "Error en la conexion a la base de datos");
-            response.sendRedirect("/SIWAI/Seccion/Sucursal/consultar.jsp");
+            request.getSession().setAttribute("msjCE", "Error en la conexion a la base de datos");
+            response.sendRedirect("/SIWAI/Seccion/Empleado/consultar.jsp");
         }
         if (lista.isEmpty()) {
-            request.getSession().setAttribute("msjCS", "No se encontro ningúna sucursal");
+            request.getSession().setAttribute("msjCE", "No se encontro ningún Empleado");
         }
-        request.getSession().setAttribute("sucursales", lista);
-        response.sendRedirect("/SIWAI/Seccion/Sucursal/consultar.jsp");
+        request.getSession().setAttribute("empleados", lista);
+        response.sendRedirect("/SIWAI/Seccion/Empleado/consultar.jsp");
+
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void iniciarSesion(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        String usuario = request.getParameter("usuario");
+        String contraseña = request.getParameter("contra");
+        Fachada fachada = new Fachada();
+        String ingreso = fachada.iniciarSesion(usuario, contraseña);
+        if (!ingreso.equalsIgnoreCase("NULL")) {
+            request.getSession().setAttribute("usuario", ingreso);
+        }
+        PrintWriter out = response.getWriter();
+        out.print(ingreso);
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -102,7 +133,6 @@ public class ControladorSucursal extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
     }
 
     /**
@@ -116,12 +146,16 @@ public class ControladorSucursal extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        if (request.getParameter("consultarSucursal") != null) {
-            consultarSucursal(request, response);
+        if (request.getParameter("consultarEmpleado") != null) {
+            consultarEmpleado(request, response);
         }
-        if (request.getParameter("registrarSucursal") != null) {
-            registrarSucursal(request, response);
+        if (request.getParameter("registrarEmpleado") != null) {
+            registrarEmpleado(request, response);
         }
+        if (request.getParameter("iniciarSesion") != null) {
+            iniciarSesion(request, response);
+        }
+
     }
 
     /**
--- SIWAI/src/java/co/edu/ufps/siwai/controlador/ControladorProveedor.java
+++ SIWAI/src/java/co/edu/ufps/siwai/controlador/ControladorProveedor.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.edu.ufps.siwai.controlador.servlet;
+package co.edu.ufps.siwai.controlador;
 
 import co.edu.ufps.siwai.modelo.fachada.Fachada;
 import co.edu.ufps.siwai.modelo.mysql.dto.ProveedorDTO;
